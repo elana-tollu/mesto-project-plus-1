611db998da61a29209912a9293c24b7da9d44c52
Elana Tollu <elana.tollu@yandex.ru>
2022-11-05 22:31:35 +0200

    add endpoint GET /users/:userId

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,8 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://mesto_user:mesto_password@localhost:27017/mestodb');
-user.find({})
-  .then((users) => console.log(users));
+user.findById('635edc8e38776faba385104f')
+  .then((user) => console.log(user));
 
 app.get('/', (req, res) => {
   res.send('Hello, Darling!');
@@ -17,6 +17,11 @@ app.get('/', (req, res) => {
 app.get('/users', (req, res) => {
   user.find({})
     .then((users) => res.send(users));
+});
+
+app.get('/users/:userId', (req, res) => {
+  user.findById(req.params.userId)
+    .then((theUser) => res.json(theUser));
 });
 
 app.listen(PORT, () => {
