eb10073f23307b26135ba1407cf1884f6e75f42c
Elana Tollu <elana.tollu@yandex.ru>
2022-11-25 21:52:36 +0200

    add validation to POST /card

--- src/routes/cards.ts
+++ src/routes/cards.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Joi } from 'celebrate';
 import { authenticate } from '../middlewares/auth';
 
 import {
@@ -17,6 +18,11 @@ router.delete('/:cardId', deleteCard);
 
 router.get('/', getAllCards);
 
-router.post('/', addCard);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().uri(),
+  }),
+}), addCard);
 
 export default router;
